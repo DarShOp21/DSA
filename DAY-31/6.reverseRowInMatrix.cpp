bf37bd83301ad2e3b3e25bb5594e72600b9d75cf
DarShOp21 <darshanayare43@google.com>
2025-06-02 23:27:28 +0530

    Added one more approach for reverse the matrix

--- DAY-31/6.reverseRowInMatrix.cpp
+++ DAY-31/6.reverseRowInMatrix.cpp
@@ -2,11 +2,29 @@
 
 using namespace std;
 
-void revMatrix(int matrix[][3] , int row , int col ){
+void revMatrix1(int matrix[][3] , int row , int col ){
     for(int i = 0 ; i < row ; i++){
         for(int j = 0 ; j < col/2 ; j++){
             swap(matrix[i][j] , matrix[i][col-j-1]);
         }
+    }
+
+    cout << endl << endl ;
+    for(int i = 0 ; i < 4 ; i++){
+        for(int j = 0 ; j < 3 ; j++){
+            cout << matrix[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+
+void revMatrix2(int matrix[][3] , int row , int col ){
+    for(int i = 0 ; i < row ; i++){
+        int start = 0 , end = col-1;
+        while (start < end){
+            swap(matrix[start], matrix[end]);
+            start++ , end--;
+        } 
     }
 
     cout << endl << endl ;
@@ -28,6 +46,7 @@ int main(){
         cout << endl;
     }
 
-    revMatrix(arr,4,3);
+    revMatrix1(arr,4,3);
+    revMatrix2(arr,4,3);
     return 0;
 }
